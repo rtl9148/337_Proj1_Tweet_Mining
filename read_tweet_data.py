ae8113dba1358cad8a9c271db30b245f7f73ffaf
WangchengXu-1 <92353907+WangchengXu-1@users.noreply.github.com>
2021-10-12 18:07:41 -0500

    Add files via upload

---
+++ read_tweet_data.py
@@ -0,0 +1,14 @@
+
+import json
+
+def read_tweet_text(file_name):
+    with open(file_name,'r') as open_file:
+        text_lines = [i['text'] for i in json.load(open_file)]
+    return text_lines
+
+if __name__ == '__main__':
+    data_file = 'gg2013.json'
+    text_lines = read_tweet_text(data_file)
+    for line_i in text_lines[:100]:
+        print(line_i)
+    
