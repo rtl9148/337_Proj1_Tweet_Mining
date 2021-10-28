4d8d039bc38729c12a400d64d0b313c158f604a3
Alexis Puente <alexispuente02@gmail.com>
2021-10-28 18:58:13 -0500

    printing text for sentiment

--- scratchwork.py
+++ scratchwork.py
@@ -8,18 +8,6 @@ import operator
 from collections import Counter
 from textblob import TextBlob
 
-#Clean tweets by removing tweets with the word 'presenter'
-#B/C When getting hosts, sometimes presenters are returned.
-def clean_tweets(data_lines):
-    filtered_tweets = []
-    for tweet in data_lines:
-        #if "rt" not in tweet and "@" not in tweet and "http" not in tweet:
-         #   filtered_tweets.append(tweet)
-         if "presenter" not in tweet:
-             filtered_tweets.append(tweet)
-    #print('Number of Clean Tweets: ', len(filtered_tweets))
-    return filtered_tweets
-
 #Load tweets into a list
 def make_tweet_list(file_name):
     with open(file_name,'r') as open_file:
@@ -27,39 +15,21 @@ def make_tweet_list(file_name):
         data_lines = [i['text'] for i in data_entries]
     return data_lines
 
-#Create dictionary with possible answers
-def extract_info(data_lines):
-    check_list = ['nominated']
-    extract_line = '\"((?:[A-Z]+[a-z]*[ ]?)+)\"'
-    #extract_line = "\w+\s\w+"
-    count_table = {}
-    for line_i in data_lines:
-        is_valid = True
-        for check_i in check_list:
-            if not re.search(check_i, line_i.lower()):
-                is_valid = False
-        if is_valid:
-            extract_keys = re.findall(extract_line,line_i)
-            for key_i in extract_keys:
-                key_normal = key_i.lower()
-                if key_normal in count_table:
-                    count_table[key_normal] += 1
-                else:
-                    count_table[key_normal] = 1
-    return count_table
-
-
-#Output the key with the most votes
-def get_max_k (count_table):
-    return max(count_table.items(), key=operator.itemgetter(1))[0]
+#timestamps 
+#"timestamps_ms"
 
 def get_keyword_tweets(keyword, data_lines):
-    host_tweets = []
+    keyword_tweets = []
     #filter tweets to include only host-related tweets
     for tweet in data_lines:
         if re.search(keyword, tweet.lower()):
-            host_tweets.append(tweet)
-    return host_tweets
+            keyword_tweets.append(tweet)
+
+    #Remove word best from tweets
+    if "best" in keyword:
+        for i in range(len(keyword_tweets)):
+            keyword_tweets[i] = keyword_tweets[i].replace("best", "")
+    return keyword_tweets
 
 def get_sentiment(tweets):
     #Find the sentiment
@@ -68,61 +38,53 @@ def get_sentiment(tweets):
         blob = TextBlob(tweet)
         polarity = blob.sentiment.polarity
         polarities.append(polarity)
-        print("Tweet: ", tweet, ", ", "Polarity: ", polarity)
+        #print("Tweet: ", tweet, ", ", "Polarity: ", polarity)
     
     average_pol = sum(polarities) / len(polarities)
 
     return average_pol
 
 
-def get_hosts(data_lines):
-    host_tweets = []
-    #filter tweets to include only host-related tweets
-    for tweet in data_lines:
-        if re.search('host', tweet.lower()):
-            host_tweets.append(tweet)
-    hosts = {}
-    ent_list = []
-    for tweet in host_tweets:
-        ent_list = get_entities(tweet)
-        str_list = str(ent_list)
-        if str_list != '[]':
-            if str_list in hosts:
-                hosts[str_list] += 1
-            else:
-                hosts[str_list] = 1
-    return hosts
+def get_sentiments_categories(tweets):
+    #Get list of tweets for each category
+    host_tweets = get_keyword_tweets("host", data_lines)
+    song_tweets = get_keyword_tweets("best original song - motion picture", data_lines)
+    animated_tweets = get_keyword_tweets("best animated feature film", data_lines)
+    cecil_tweets = get_keyword_tweets("cecil b. demille award", data_lines)
 
-def get_entities(tweet):
-    doc = nlp(tweet)
-    ent_list = []
-    for entity in doc.ents:
-        if entity.label_ == 'PERSON':
-            if 'golden globe' not in entity.text.lower(): #golden globe is labeled as person
-                ent_list.append(entity.text)
-    #if ent_list:
-    #    print(ent_list)
-    return ent_list
+    #Get the sentiment for each category
+    host_sentiment = get_sentiment(host_tweets)
+    song_sentiment = get_sentiment(song_tweets)
+    anim_sentiment = get_sentiment(animated_tweets)
+    cecil_sentiment = get_sentiment(cecil_tweets)
+
+    #Print out sentiment in text
+    print_sentiment(host_sentiment, "Host")
+    print_sentiment(song_sentiment, "Best Original Song - Motion Picture")
+    print_sentiment(anim_sentiment, "Best Animated Feature Film")
+    print_sentiment(cecil_sentiment, "Cecil B. Demille Award")
+
+
+def print_sentiment(sentiment, category):
+    if sentiment >= .5:
+        print("Sentiment for ", category, " is strongly positive (Polarity Score: ", sentiment)
+    elif sentiment > 0 and sentiment < .5:
+        print("Sentiment for ", category, " is weakly positive (Polarity Score: ", sentiment)
+    elif sentiment == 0:
+        print("Sentiment for ", category, " is neutral (Polarity Score: ", sentiment)
+    elif sentiment < 0 and sentiment > -.5:
+        print("Sentiment for ", category, " is weakly negative (Polarity Score: ", sentiment)
+    elif sentiment <= -.5:
+        print("Sentiment for ", category, " is strongly negative (Polarity Score: ", sentiment)
+
 
 
 
 if __name__ == '__main__':
     data_file = 'gg2013.json'
     data_lines = make_tweet_list(data_file)
-    #filtered = clean_tweets(data_lines)
-   # hosts = get_hosts(filtered)
-    #print(hosts)
-    #print('Host: ', get_max_k(hosts))
-    host_tweets = get_keyword_tweets("host", data_lines)
-    song_tweets = get_keyword_tweets("best original song - motion picture", data_lines)
-    animated_tweets = get_keyword_tweets("best animated feature film", data_lines)
-    cecil_tweets = get_keyword_tweets("cecil b. demille award", data_lines)
-    #song_sentiment = get_sentiment(song_tweets)
 
-    #print("Number of Tweets: ", len(host_tweets))
-    #print("Best Original Song Sentiment: ", get_sentiment(song_tweets))
-    print("Best animated feature film sentiment: ", get_sentiment(animated_tweets))
-    #print("Cecil b Demille Sentiment: ", get_sentiment(cecil_tweets))
+    get_sentiments_categories(data_lines)
 
     
 
