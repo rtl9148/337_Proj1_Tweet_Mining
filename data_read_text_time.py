d897e54e225a17f6d0aaa4fd4301d945f0184d17
WangchengXu-1 <92353907+WangchengXu-1@users.noreply.github.com>
2021-10-28 18:48:09 -0500

    Add files via upload
    Example loading text with time stamps and sorting them based on the time.

--- data_read_text_time.py
+++ data_read_text_time.py
@@ -11,6 +11,8 @@ if __name__ == '__main__':
     data_file = 'gg{}.json'.format(data_year)
     with open(data_file,'r') as open_file:
         data_entry = json.load(open_file)
-    data_text = [i['text'] for i in data_entry]
-    data_time = [i['timestamp_ms'] for i in data_entry]
+    data_time_text = [(i['timestamp_ms'],i['text']) for i in data_entry]
+    data_time_text.sort()
+    
+    sorted_text = [i[1] for i in data_time_text]
 
