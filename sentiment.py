d73f5061ab42656007aa5b782e1b11dd62e24751
Alexis Puente <alexispuente02@gmail.com>
2021-10-31 16:04:28 -0500

    finishing up sentiment analysis

---
+++ sentiment.py
@@ -0,0 +1,111 @@
+import json
+import re
+import os
+import operator
+from collections import Counter
+from textblob import TextBlob
+import matplotlib.pyplot as plt
+
+test_dictionary = {"hosts": ["amy poehler", "tina fey"], "award_data": {"best screenplay - motion picture": {"nominees": ["zero dark thirty", "lincoln", "silver linings playbook", "argo"], "presenters": ["robert pattinson", "amanda seyfried"], "winner": "django unchained"}, "best director - motion picture": {"nominees": ["kathryn bigelow", "ang lee", "steven spielberg", "quentin tarantino"], "presenters": ["halle berry"], "winner": "ben affleck"}, "best performance by an actress in a television series - comedy or musical": {"nominees": ["zooey deschanel", "tina fey", "julia louis-dreyfus", "amy poehler"], "presenters": ["aziz ansari", "jason bateman"], "winner": "lena dunham"}, "best foreign language film": {"nominees": ["the intouchables", "kon tiki", "a royal affair", "rust and bone"], "presenters": ["arnold schwarzenegger", "sylvester stallone"], "winner": "amour"}, "best performance by an actor in a supporting role in a motion picture": {"nominees": ["alan arkin", "leonardo dicaprio", "philip seymour hoffman", "tommy lee jones"], "presenters": ["bradley cooper", "kate hudson"], "winner": "christoph waltz"}, "best performance by an actress in a supporting role in a series, mini-series or motion picture made for television": {"nominees": ["hayden panettiere", "archie panjabi", "sarah paulson", "sofia vergara"], "presenters": ["dennis quaid", "kerry washington"], "winner": "maggie smith"}, "best motion picture - comedy or musical": {"nominees": ["the best exotic marigold hotel", "moonrise kingdom", "salmon fishing in the yemen", "silver linings playbook"], "presenters": ["dustin hoffman"], "winner": "les miserables"}, "best performance by an actress in a motion picture - comedy or musical": {"nominees": ["emily blunt", "judi dench", "maggie smith", "meryl streep"], "presenters": ["will ferrell", "kristen wiig"], "winner": "jennifer lawrence"}, "best mini-series or motion picture made for television": {"nominees": ["the girl", "hatfields & mccoys", "the hour", "political animals"], "presenters": ["don cheadle", "eva longoria"], "winner": "game change"}, "best original score - motion picture": {"nominees": ["argo", "anna karenina", "cloud atlas", "lincoln"], "presenters": ["jennifer lopez", "jason statham"], "winner": "life of pi"}, "best performance by an actress in a television series - drama": {"nominees": ["connie britton", "glenn close", "michelle dockery", "julianna margulies"], "presenters": ["nathan fillion", "lea michele"], "winner": "claire danes"}, "best performance by an actress in a motion picture - drama": {"nominees": ["marion cotillard", "sally field", "helen mirren", "naomi watts", "rachel weisz"], "presenters": ["george clooney"], "winner": "jessica chastain"}, "cecil b. demille award": {"nominees": [], "presenters": ["robert downey, jr."], "winner": "jodie foster"}, "best performance by an actor in a motion picture - comedy or musical": {"nominees": ["jack black", "bradley cooper", "ewan mcgregor", "bill murray"], "presenters": ["jennifer garner"], "winner": "hugh jackman"}, "best motion picture - drama": {"nominees": ["django unchained", "life of pi", "lincoln", "zero dark thirty"], "presenters": ["julia roberts"], "winner": "argo"}, "best performance by an actor in a supporting role in a series, mini-series or motion picture made for television": {"nominees": ["max greenfield", "danny huston", "mandy patinkin", "eric stonestreet"], "presenters": ["kristen bell", "john krasinski"], "winner": "ed harris"}, "best performance by an actress in a supporting role in a motion picture": {"nominees": ["amy adams", "sally field", "helen hunt", "nicole kidman"], "presenters": ["megan fox", "jonah hill"], "winner": "anne hathaway"}, "best television series - drama": {"nominees": ["boardwalk empire", "breaking bad", "downton abbey (masterpiece)", "the newsroom"], "presenters": ["salma hayek", "paul rudd"], "winner": "homeland"}, "best performance by an actor in a mini-series or motion picture made for television": {"nominees": ["benedict cumberbatch", "woody harrelson", "toby jones", "clive owen"], "presenters": ["jessica alba", "kiefer sutherland"], "winner": "kevin costner"}, "best performance by an actress in a mini-series or motion picture made for television": {"nominees": ["nicole kidman", "jessica lange", "sienna miller", "sigourney weaver"], "presenters": ["don cheadle", "eva longoria"], "winner": "julianne moore"}, "best animated feature film": {"nominees": ["frankenweenie", "hotel transylvania", "rise of the guardians", "wreck-it ralph"], "presenters": ["sacha baron cohen"], "winner": "brave"}, "best original song - motion picture": {"nominees": ["act of valor", "stand up guys", "the hunger games", "les miserables"], "presenters": ["jennifer lopez", "jason statham"], "winner": "skyfall"}, "best performance by an actor in a motion picture - drama": {"nominees": ["richard gere", "john hawkes", "joaquin phoenix", "denzel washington"], "presenters": ["george clooney"], "winner": "daniel day-lewis"}, "best television series - comedy or musical": {"nominees": ["the big bang theory", "episodes", "modern family", "smash"], "presenters": ["jimmy fallon", "jay leno"], "winner": "girls"}, "best performance by an actor in a television series - drama": {"nominees": ["steve buscemi", "bryan cranston", "jeff daniels", "jon hamm"], "presenters": ["salma hayek", "paul rudd"], "winner": "damian lewis"}, "best performance by an actor in a television series - comedy or musical": {"nominees": ["alec baldwin", "louis c.k.", "matt leblanc", "jim parsons"], "presenters": ["lucy liu", "debra messing"], "winner": "don cheadle"}}}
+
+#Load tweets into a list, includes timestamps and text in a tuple
+def read_tweet_time(file_name):
+    with open(file_name,'r') as open_file:
+        data_entry = json.load(open_file)
+    data_time_text = [(i['timestamp_ms'],i['text']) for i in data_entry]
+    data_time_text.sort()
+    
+    sorted_text = [i[1] for i in data_time_text]
+    return data_time_text
+
+#Looks for tweets that contain the given keyword
+def get_keyword_tweets(keyword, data_lines):
+    keyword_tweets = []
+    for tweet in data_lines:
+        if re.search(keyword, tweet[1].lower()):
+            if "best" in keyword:
+                keyword_tweets.append((tweet[0], tweet[1].replace("best","")))
+            else:
+                keyword_tweets.append(tweet)
+
+    return keyword_tweets
+
+#Finds the average sentiment for the given category, and plots it over time
+def get_sentiment(tweets, category):
+    polarities = []
+    timestamps = []
+    print(len(tweets))
+    for tweet in tweets:
+        blob = TextBlob(tweet[1])
+        polarity = blob.sentiment.polarity
+        avg_pol = (sum(polarities) + polarity) / (len(polarities) + 1)
+        polarities.append(avg_pol)
+        timestamps.append(tweet[0])
+
+    average_pol = sum(polarities) / len(polarities)
+
+    plt.plot(timestamps, polarities)
+    print(type(timestamps[0]))
+    plt.title("Average Sentiment Over Time for " + category)
+    plt.xlabel("Time (ms)")
+    plt.ylabel("Average Sentiment")
+    plt.show()
+
+    return average_pol
+
+#Prints the polarity score along with a brief analysis
+def print_sentiment(sentiment, category):
+    if sentiment >= .5:
+        print("Sentiment for ", category, " is strongly positive (Polarity Score: ", sentiment)
+    elif sentiment > 0 and sentiment < .5:
+        print("Sentiment for ", category, " is weakly positive (Polarity Score: ", sentiment)
+    elif sentiment == 0:
+        print("Sentiment for ", category, " is neutral (Polarity Score: ", sentiment)
+    elif sentiment < 0 and sentiment > -.5:
+        print("Sentiment for ", category, " is weakly negative (Polarity Score: ", sentiment)
+    elif sentiment <= -.5:
+        print("Sentiment for ", category, " is strongly negative (Polarity Score: ", sentiment)
+
+#Combines all previous functions. Finds keywords related tweets, finds the sentiment, and prints out text
+#Specifically looks for tweets related to hosts, and the presenters, nominees, and winner for each award
+def get_sentiments_categories(tweets, my_dictionary):
+    host_tweets = []
+    for i in my_dictionary["hosts"]:
+        host_tweets.extend(get_keyword_tweets(i, tweets))
+    host_tweets.sort()    
+    host_sentiment = get_sentiment(host_tweets, "Host")
+    print_sentiment(host_sentiment, "Host")
+
+    awards = my_dictionary["award_data"].keys()
+
+    for i in awards:
+        nominee_tweets = []
+        for nominee in my_dictionary["award_data"][i]["nominees"]:
+            nominee_tweets.extend(get_keyword_tweets(nominee, tweets))
+
+        presenters_tweets = []
+        for presenter in my_dictionary["award_data"][i]["presenters"]:
+            presenters_tweets.extend(get_keyword_tweets(presenter, tweets))
+
+        winner_tweets = get_keyword_tweets(my_dictionary["award_data"][i]["winner"], tweets)
+        nominee_tweets.sort()
+        nominee_sentiment = get_sentiment(nominee_tweets, "Nominees - " + i)
+        print_sentiment(nominee_sentiment, "Nominees - " + i)
+        winner_tweets.sort()
+        winner_sentiment = get_sentiment(winner_tweets, "Winner - " + i)
+        print_sentiment(winner_sentiment, "Winner - " + i)
+        presenters_tweets.sort()
+        presenter_sentiment = get_sentiment(presenters_tweets, "Presenters - " + i)
+        print_sentiment(presenter_sentiment, "Presenters - " + i)
+
+
+if __name__ == '__main__':
+    data_file = 'gg2013.json'
+    data_lines = read_tweet_time(data_file)
+    get_sentiments_categories(data_lines, test_dictionary)
+
+    
+
+    
+
+
